f5acdcce3d54f67a158f8679a30fbfbfbb7a9876
David Leal Peláez <david.pelaez@vtex.com.br>
2021-07-08 17:06:34 -0500

    feature/Done images jumbotron

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import Header from './components/Header'
-import Jumbotron from './components/Jumbotron'
-import { Button } from 'reactstrap';
-import './styles.css'
+import React from "react";
+import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
+import "./styles.css";
 
 export default function App() {
-    return (
-        <div>
-            <Header />
-            <Jumbotron />
-            <Button color="danger">Danger!</Button>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Jumbotron />
+    </div>
+  );
 }
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,13 +5,13 @@ import jumbotronImage from '../assets/header-x1.png'
 export default function jumbotron() {
     return (
         <div className="container__jumbotron">
-                {/*eslint-disable-next-line */}
-                <img
-                    width="100%"
-                    height="480"
-                    src={jumbotronImage}
-                />
-                <p className="jumbotron__text">Electronics</p>
+            {/*eslint-disable-next-line */}
+            <img
+                width="100%"
+                height="480"
+                src={jumbotronImage}
+            />
+            <div className="jumbotron__text">Electronics</div>
         </div>
     )
 }
